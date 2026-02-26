1ac4836c5106d8f9ee718e8b5ded9e08e769b406
cesarzb <cesarzb@protonmail.com>
2026-02-26 13:10:13 +0100

    Improved area of layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,8 +6,8 @@ const Layout = () => {
     <div className="flex bg-stone-900 min-h-full text-slate-200">
       <Sidebar />
 
-      <main className="flex-1 h-screen overflow-y-auto p-6 lg:p-10">
-        <div className="max-w-7xl mx-auto">
+      <main className="flex-1 h-full overflow-y-auto p-6 lg:p-10">
+        <div className="max-w-7xl mx-auto h-full">
           <Outlet />
         </div>
       </main>
