a3f14eeb1a07dd83c573cc6bd499d290a0076d15
cesarzb <cesarzb@protonmail.com>
2026-03-26 12:00:12 +0100

    feat(prod): added early wake-up call to free backend instance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,23 @@
 
 import Link from "next/link";
 import Hourglass from "@/components/Timer/Hourglass";
-import ActionButton from "../components/ui/ActionButton/ActionButton";
+import ActionButton from "@/components/ui/ActionButton/ActionButton";
+import api from "@/api/client";
+import { useEffect } from "react";
 
 function App() {
+  // hack to wake up free backend instance in the background
+  useEffect(() => {
+    const wakeUpBackend = async () => {
+      try {
+        api.get("/");
+        console.log("Wake-up signal sent to NestJS");
+      } catch (e) {}
+    };
+
+    wakeUpBackend();
+  }, []);
+
   return (
     <div className="flex flex-col justify-around items-center h-full">
       <Hourglass time={5 * 60} fullPomodoroLength={25 * 60} />
@@ -12,9 +26,6 @@ function App() {
       <div className="flex gap-4 text-white">
         <Link href="/login">
           <ActionButton label="Login" className="w-36" variant="primary" />
-        </Link>
-        <Link href="/register">
-          <ActionButton label="Register" className="w-36" variant="secondary" />
         </Link>
       </div>
     </div>
