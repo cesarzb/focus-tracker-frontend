a84d10f6f36aff63ca9ca16da6da7df113a6c3ce
cesarzb <cesarzb@protonmail.com>
2026-04-27 17:20:03 +0200

    refactor(FormInput): simplified tailwind class

--- src/components/ui/Form/FormInput.tsx
+++ src/components/ui/Form/FormInput.tsx
@@ -20,7 +20,7 @@ const FormInput = ({
       )}
       <input
         {...props}
-        className={`w-full rounded-xl border border-stone-700/50 bg-stone-950 p-3 text-stone-50 outline-none transition-all focus:border-orange-500/50 focus:ring-4 focus:ring-orange-500/10 [color-scheme:dark] disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
+        className={`w-full rounded-xl border border-stone-700/50 bg-stone-950 p-3 text-stone-50 outline-none transition-all focus:border-orange-500/50 focus:ring-4 focus:ring-orange-500/10 scheme-dark disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
       />
       {error && <p className="mt-1 text-xs text-red-400">{error}</p>}
     </div>
